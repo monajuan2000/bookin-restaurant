f0e1e72ed93dbdd2bfa909feb853fa78be674e2a
monajuan2000 <monajuan2000@gmail.com>
2022-01-06 20:33:14 -0500

    adding changes to the package structure and adding entity classes

--- booking-restaurant-api/src/main/java/com/ias/bookingrestaurantapi/infrastructure/persistence/entities/RestaurantEntity.java
+++ booking-restaurant-api/src/main/java/com/ias/bookingrestaurantapi/infrastructure/persistence/entities/RestaurantEntity.java
@@ -1,4 +1,33 @@
 package com.ias.bookingrestaurantapi.infrastructure.persistence.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "restaurant")
 public class RestaurantEntity {
+
+    public RestaurantEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", unique = true, nullable = true)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "IMAGE")
+    private String image;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "restaurant")
+    private List<ReservationEntity> reservations;
+
 }
