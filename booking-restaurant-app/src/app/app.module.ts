bacbdfb0b31bbe3e5c38754c614a56ccf0d739fd
monajuan2000 <monajuan2000@gmail.com>
2022-04-07 11:33:24 -0500

    add components and rout

--- booking-restaurant-app/src/app/app.module.ts
+++ booking-restaurant-app/src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { ExploreComponent } from './components/explore/explore.component';
+import { CancelBookingComponent } from './components/cancel-booking/cancel-booking.component';
+import { HeaderComponent } from './components/header/header.component';
+
+const appRoutes: Routes = [
+   {path: '', component: ExploreComponent},
+   {path: 'booking', component: BookingComponent},
+   {path: 'cancel', component: CancelBookingComponent},
+   {path: '', component: ExploreComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent,
+    ExploreComponent,
+    CancelBookingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
